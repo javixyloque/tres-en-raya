b388eabd2bf5c9211488332db55b13599d077c07
javixyloque <perosa1000@gmail.com>
2024-10-25 13:43:15 +0200

    25/10 clase

--- app.js
+++ app.js
@@ -1,81 +1,106 @@
 "use strict";
 
-const CASILLAS_VALIDAS = ['1', '2', '3', '4', '5', '6', '7', '8', '9',];
-const FICHAS = ['X', 'O',];
-
-const FILA_IZQUIERDA = [1, 4, 7];
+let turnoActual = 0;
+const CASILLAS_VALIDAS = ["1","2","3","4","5","6","7","8","9"]
+const FICHAS = ["X","O"];
+const combinacionesGanadoras = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 5, 9],
+    [3, 5, 7],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9]
+]
+const FILA_IZQUIERDA = [1,4,7]
 const FILA_DERECHA = [3, 6, 9];
 const DIAGONAL_PRINCIPAL = [1, 5, 9];
 const DIAGONAL_SECUNDARIA = [3, 5, 7];
-
 let victoria = false;
 let tablas = false;
-let turnoActual = 0;
 
-
-function comprobarCasillaValida(casilla) {
-    let contenido = casilla.textContent;
-    return CASILLAS_VALIDAS.includes(contenido);
-}
-
-function ejecutarTurno(casilla) {
-    casilla.textContent = FICHAS[turnoActual % 2];
-    turnoActual++;
-}
-
-function comprobarTablas() {
-    if(turnoActual == 9 && !victoria) {
-        tablas = true;
-    }
-}
-
-function comprobarHorizontal() {}
-function comprobarVertical() {}
-function comprobarDiagonalPrincipal() {}
-function comprobarDiagonalSecundaria() {}
-
-function comprobarFinDeJuego(casilla) {
-    const numeroCasilla = casilla.textContent;
-
-    comprobarHorizontal(numeroCasilla);
-    comprobarVertical(numeroCasilla);
-    if(DIAGONAL_PRINCIPAL.includes(numeroCasilla)) {
-        comprobarDiagonalPrincipal(numeroCasilla);
-    }
-    if(DIAGONAL_SECUNDARIA.includes(numeroCasilla)) {
-        comprobarDiagonalSecundaria(numeroCasilla);
-    }
-
-    comprobarTablas();
-
-    if(victoria) {
-        alert('Gana ' + FICHAS[turnoActual % 2]);
-        return;
-    }
-
-    if(tablas) {
-        alert('Tablas');
-        return;
-    }
-}
-
-function casillaOnClick(event) {
-    let casilla = event.target;
-    console.log("click en casilla "+ casilla.textContent);
-
-    if(comprobarCasillaValida(casilla)) {
-        ejecutarTurno(casilla);
-        comprobarFinDeJuego(casilla);
-    }
-}
 
 function main() {
-    for(let i = 1; i <= 9; i++) {
-//        let casilla = document.getElementById(`casilla-${i}`);
-        let casilla = document.querySelector(`#casilla-${i}`);
-        casilla.addEventListener('click', casillaOnClick);
+    for (let i = 1; i<=9; i++) {
+        let casilla = document.getElementById(`casilla-${i}`);
+        casilla.addEventListener("click", casillaOnClick);
+    }
+    
+
+    
+};
+
+function casillaOnClick (e) {
+
+    let casilla = e.target;
+
+    if (comprobarCasillaValida(casilla)) {
+        ejecutarTurno(casilla);
+        comprobarFinJuego(casilla);
+    } else {
+
     }
 
+}
+
+function comprobarCasillaValida(casilla) {
+    let contenido = casilla.textContent
+    return CASILLAS_VALIDAS.includes(contenido);
+    
+}
+
+function ejecutarTurno (casilla) {
+    const numeroCasilla = casilla.textContent; 
+    casilla.textContent = FICHAS[ turnoActual  %  2  ] ;
+    turnoActual++;
+}
+function comprobarTablero(casilla) {
+    // let contador = 0;
+    const letraAct = FICHAS[turnoActual%2];
+    // NECESITARIA HACER UN TABLERO APARTE PARA HACER ESTO :')
+    for (let ganar of combinacionesGanadoras) {
+        victoria = ganar.every((i) => i.textContent = letraAct);
+    }
+        
+        
+
+
+    //     if (FICHAS[turno]) {
+
+    //     }
+    
+    return victoria;
+}
+
+
+function comprobarTablas (casilla) {
+    if (turnoActual ==9&&!victoria) {
+        alert("Empate")
+    }
+}
+function comprobarFinJuego (casilla) {
+    // const numeroCasilla = casilla.id.split("-")[1];
+    comprobarTablero(casilla)
+    // comprobarVertical(numeroCasilla);
+    // if (DIAGONAL_SECUNDARIA.includes(numeroCasilla)) {
+    //     comprobarDiagonalPrincipal`numeroCasilla`;
+    // }
+    // if (DIAGONAL_SECUNDARIA.includes(numeroCasilla)) {
+    //     comprobarDiagonalSecundaria(numeroCasilla);
+    // }
+
+    comprobarTablas(casilla);
+
+    if  (victoria) {
+        //EMPATE
+        alert (`Gana el jugador ${FICHAS[ turnoActual  %  2  ]}`)
+    }
+    
+
+    if (tablas) {
+        alert("Tablas")
+    }
 }
 
 main();
