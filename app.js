bee73bc0cff0984a94c409c0a7a2c155afebb6b6
javixyloque <perosa1000@gmail.com>
2024-11-04 11:57:16 +0100

    Cambios clase comprobar victoria Javier Alvarez Centeno

--- app.js
+++ app.js
@@ -1,22 +1,19 @@
 "use strict";
 
 let turnoActual = 0;
+let tablero = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+];
+
 const CASILLAS_VALIDAS = ["1","2","3","4","5","6","7","8","9"]
 const FICHAS = ["X","O"];
-const combinacionesGanadoras = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-    [1, 5, 9],
-    [3, 5, 7],
-    [1, 4, 7],
-    [2, 5, 8],
-    [3, 6, 9]
-]
-const FILA_IZQUIERDA = [1,4,7]
-const FILA_DERECHA = [3, 6, 9];
-const DIAGONAL_PRINCIPAL = [1, 5, 9];
-const DIAGONAL_SECUNDARIA = [3, 5, 7];
+
+// const FILA_IZQUIERDA = [1,4,7]
+// const FILA_DERECHA = [3, 6, 9];
+// const DIAGONAL_PRINCIPAL = [1, 5, 9];
+// const DIAGONAL_SECUNDARIA = [3, 5, 7];
 let victoria = false;
 let tablas = false;
 
@@ -38,9 +35,7 @@ function casillaOnClick (e) {
     if (comprobarCasillaValida(casilla)) {
         ejecutarTurno(casilla);
         comprobarFinJuego(casilla);
-    } else {
-
-    }
+    } 
 
 }
 
@@ -51,50 +46,71 @@ function comprobarCasillaValida(casilla) {
 }
 
 function ejecutarTurno (casilla) {
-    const numeroCasilla = casilla.textContent; 
+    const numeroCasilla = casilla.id.split("-")[1];
+    if (numeroCasilla<=3&&tablero[0][numeroCasilla-1]=='') {
+        tablero[0][numeroCasilla-1] = FICHAS[turnoActual%2];
+    }else if (numeroCasilla <= 6 && tablero[1][numeroCasilla-4]=='') {
+        tablero[1][numeroCasilla-4] = FICHAS[turnoActual%2];
+    } else if (numeroCasilla >= 7 && tablero[2][numeroCasilla-7]=='')  {
+        tablero[2][numeroCasilla-7] = FICHAS[turnoActual%2];
+    }
     casilla.textContent = FICHAS[ turnoActual  %  2  ] ;
     turnoActual++;
 }
-function comprobarTablero(casilla) {
+function comprobarTablero(tablero) {
     // let contador = 0;
-    const letraAct = FICHAS[turnoActual%2];
-    // NECESITARIA HACER UN TABLERO APARTE PARA HACER ESTO :')
-    for (let ganar of combinacionesGanadoras) {
-        victoria = ganar.every((i) => i.textContent = letraAct);
-    }
-        
-        
-
-
-    //     if (FICHAS[turno]) {
-
-    //     }
+    const combinacionesGanadoras = [
+        // Pongo estos comentarios pq si no me vuelvo loco 
+             // HORIZONTALES
+            [tablero[0][0], tablero[0][1], tablero[0][2]],
+            [tablero[1][0], tablero[1][1], tablero[1][2]],
+            [tablero[2][0], tablero[2][1], tablero[2][2]],
+            //DIAGONALES
+            [tablero[0][0], tablero[1][1], tablero[2][2]],
+            [tablero[2][0], tablero[1][1], tablero[0][2]],
+            //VERTICALES
+            [tablero[0][0], tablero[1][0], tablero[2][0]],
+            [tablero[0][1], tablero[1][1], tablero[2][1]],
+            [tablero[0][2], tablero[1][2], tablero[2][2]]
+    ]
     
+
+    for (let ganar of combinacionesGanadoras) {
+        if (ganar[0]===ganar[1] && ganar[0] === ganar[2] && ganar[0] !== ''){
+            victoria =  true;
+            return victoria;
+        } else {
+            victoria = false;
+        }
+        
+    }
     return victoria;
+        
+
+
+
+    
+    
 }
 
 
-function comprobarTablas (casilla) {
-    if (turnoActual ==9&&!victoria) {
+
+function comprobarTablas () {
+    if (turnoActual == 9 && !comprobarTablero(tablero)) {
         alert("Empate")
     }
 }
+
 function comprobarFinJuego (casilla) {
-    // const numeroCasilla = casilla.id.split("-")[1];
-    comprobarTablero(casilla)
-    // comprobarVertical(numeroCasilla);
-    // if (DIAGONAL_SECUNDARIA.includes(numeroCasilla)) {
-    //     comprobarDiagonalPrincipal`numeroCasilla`;
-    // }
-    // if (DIAGONAL_SECUNDARIA.includes(numeroCasilla)) {
-    //     comprobarDiagonalSecundaria(numeroCasilla);
-    // }
+    const numeroCasilla = casilla.id.split("-")[1];
+    console.log(comprobarTablero(tablero));
+    
 
-    comprobarTablas(casilla);
-
-    if  (victoria) {
-        //EMPATE
-        alert (`Gana el jugador ${FICHAS[ turnoActual  %  2  ]}`)
+    comprobarTablas();
+    console.log(tablero)
+    if  (comprobarTablero(tablero)) {
+        // VICTORIA
+        alert (`Gana el jugador ${FICHAS[ (turnoActual  %  2) -1  ]}`)
     }
     
 
